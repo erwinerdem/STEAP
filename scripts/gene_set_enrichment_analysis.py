7757ab5a01c2320f13514fd8580140c7ad41b2cf
erwinerdem <erwin.erdem@live.nl>
2021-03-23 14:26:04 +0100

    Updated GSEA

--- scripts/gene_set_enrichment_analysis.py
+++ scripts/gene_set_enrichment_analysis.py
@@ -10,33 +10,34 @@ import mygene
 import gseapy
 from pathlib import Path
 
-def get_annot_list(df, name, param_list):
+def get_annot_list(df, name, param_list, rank):
     param = '|'.join(param_list)
     df_gsea = pd.concat([df[(df['gwas'].str.contains(param))][['gwas','specificity_id','annotation']],
                        (df[(df['gwas'].str.contains(param))][f'pvalue_{constants.PVAL_CORRECTION}'] <= 0.05).astype(int)
                        ], axis=1).rename(columns={f'pvalue_{constants.PVAL_CORRECTION}':'count'}).groupby(['gwas','specificity_id','annotation']).sum()
-
+    n_gwas = df_gsea.index.unique(level='gwas').shape[0]
     # only use count==1 if multiple gwases are used in analysis
-    original_shape = df[(df['gwas'].str.contains(param))].shape
-    if original_shape[0] == df.shape[0]:
+    if n_gwas == 1:
         min_count = 0 # if only one gwas in gwas_group than include all cell-types
     else:
-        min_count = 1
-
-    df_gsea = df_gsea[df_gsea['count']>=2]
-    df_gsea['count'] = 1
+        min_count = 1 # if multiple gwas, cell type should be included in at least 2
+        min_count = 0
+    original_shape = df[(df['gwas'].str.contains(param))].shape
+    df_gsea = df_gsea[df_gsea['count']>=2] # only get if significant in >2 methods
+    df_gsea['count'] = 1 # reset count to 1
     df_gsea = df_gsea.groupby(['specificity_id','annotation']).sum().reset_index()
     N_total = df[(df['gwas'].str.contains(param))]['gwas'].unique().shape[0]
     df_gsea['freq'] = df_gsea['count'] / N_total
     df_gsea.sort_values('freq', ascending=False, inplace=True)
     df_gsea['rank'] = df_gsea['count'].rank(ascending=False,method='dense').astype(int)
-
-
-    df_gsea = df_gsea[(df_gsea['rank']<=constants.TOP_ANNOT)&
-                      (df_gsea['count']>min_count)
-                     ]
+    if rank is None:
+        df_gsea = df_gsea[(df_gsea['count']>min_count)]
+    else:
+        df_gsea = df_gsea[(df_gsea['rank']<=rank)&
+                          (df_gsea['count']>min_count)
+                         ]
     annot_list = df_gsea[['specificity_id','annotation']].values.tolist()
-    print(f"Top {constants.TOP_ANNOT} ranked cell-types (N={len(annot_list)}) in {name} GWAS:")
+    print(f"Top {rank} ranked cell-types (N={len(annot_list)}) in {name} GWAS:")
     for row in df_gsea.iterrows():
         a = row[1]
         print(f"{a[4]}. {a[0]}: {a[1]} (N={a[2]}, freq={a[3]:.2})")
@@ -81,7 +82,8 @@ def get_top_genes(annot_list):
             celltype_genes_dict[celltype] = gene_list
     return celltype_genes_dict
 
-def summarize_gsea(gsea_dict, save_to_excel=False, filename=None):
+def summarize_gsea(gsea_dict, min_count=0, save_to_excel=False, filename=None):
+    total = len(gsea_dict)
     df_list = []
     for k,v in gsea_dict.items():
         df = v.copy()
@@ -89,17 +91,24 @@ def summarize_gsea(gsea_dict, save_to_excel=False, filename=None):
         df_list.append(df)
     gsea_df = pd.concat(df_list)
     gsea_grouped_df = gsea_df.groupby(['Gene_set','Term'])['Celltype'].agg(list).reset_index()
-    gsea_grouped_df['Celltype_count'] = gsea_grouped_df['Celltype'].apply(lambda x: len(x))
+    gsea_grouped_df[f'Celltype_count (total={total})'] = gsea_grouped_df['Celltype'].apply(lambda x: len(x))
+    gsea_grouped_df.sort_values(f'Celltype_count (total={total})', ascending=False, inplace=True)
     gsea_grouped_df['Celltype'] = gsea_grouped_df['Celltype'].apply(lambda x : '; '.join(x))
-    gsea_grouped_df.sort_values('Celltype_count', ascending=False, inplace=True)
+    gsea_grouped_df = gsea_grouped_df[gsea_grouped_df[f'Celltype_count (total={total})']>min_count]
     if save_to_excel:         
         gsea_grouped_df.to_excel(filename, index=False)
     return gsea_grouped_df
 
-def gsea(df, gwas_group_dict):
+def gsea(df, gwas_group_dict, rank=constants.TOP_ANNOT):
+    '''
+    GSEA on enriched cell types.
+    rank: The rank a cell type should have to be analysed. 
+    For example rank=5 means that only cell types ranked in the top 5 most occuring enriched cell types will be analysed.
+    If rank=None, then all cell types will be analysed.
+    '''
     print(f"Performing GSEA...\n")
     for name, param_list in gwas_group_dict.items():
-        annot_list = get_annot_list(df, name, param_list)
+        annot_list = get_annot_list(df, name, param_list, rank)
         celltype_genes_dict = get_top_genes(annot_list)
  
         print('\nRunning Enrichr...')
