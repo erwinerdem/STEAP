c6049ef510bd9f71ecab6953ddc49d4ba88b9324
Erwin Erdem <41726224+erwinerdem@users.noreply.github.com>
2021-04-27 13:38:55 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-conda env create -f environment_steap.yml # create conda env needed for running STEAP
 git lfs install 
 git lfs clone --recurse-submodules https://github.com/perslab/CELLECT.git 
 rsync -a --remove-source-files STEAP/* CELLECT
 rm -rf STEAP 
 mv CELLECT/ STEAP/
 cd STEAP
+conda env create -f environment_steap.yml # create conda env needed for running STEAP
 conda env create -f ldsc/environment_munge_ldsc.yml # create the conda env for GWAS munging
-
